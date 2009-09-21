64e7dd961d76b35d56c9d09738860a2320f5e92f
Karsten Lang <karstenlang@gmail.com>
2009-09-21 16:15:17 +0200

    calculator example, with start/stop of server

---
+++ compojure/calc.clj
@@ -0,0 +1,38 @@
+(ns calc)
+(use 'compojure)
+
+(def op-mapping
+     {"add" +
+      "subtract" -
+      "multiply" *
+      "divide" / })
+
+(defn doop [op ls rs]
+  "Do the operation on the two values"
+  (if (contains? op-mapping op)
+    (str ((get op-mapping op) ls rs))
+    "Unrecognized operation"))
+
+(defn hello-world []   
+  (html [:head 
+	 [:title "Hello World"]]
+	[:body [:h1 "Hello there"]]))
+
+(defroutes calc
+  (GET "/:op/:ls/:rs" 
+    (doop (params :op) 
+	  (. Float valueOf (params :ls))
+	  (. Float valueOf (params :rs))))
+  (GET "/i/:op/:ls/:rs" 
+    (doop (params :op) 
+	  (. Integer valueOf (params :ls))
+	  (. Integer valueOf (params :rs))))
+  (GET "/hello" (hello-world))
+  (ANY "/*" "Bad URL"))
+
+(defserver calc-server
+   {:port 8082}
+   "/*" (servlet calc))
+
+(start calc-server)
+;; --> (stop calc-server)
