0fbc1b29f0ad0dfe8f8f4543cc23e5916ff57420
Karsten Lang <karstenlang@gmail.com>
2009-09-21 16:09:00 +0200

    adding ./karsten/index.clj to this file

--- compojure/lang/index.clj
+++ compojure/lang/index.clj
@@ -1,6 +1,53 @@
 (ns lang)
 (use 'compojure)
-(load-file "./karsten/index.clj")
+
+(defn last-changed []
+  (javascript-tag 
+   (html 
+    "document.write(\"" 
+    [:hr] 
+    [:center [:h6 [:i "Last changed on \"+document.lastModified+\"" ]]] 
+    "\");")))
+
+
+(defn statusText []
+  (javascript-tag 
+   "function statusText(text) {setTimeout('top.window.status=\"'+text+'\";',1);}"))
+
+;; also defined in ./karsten/index.clj, based on ./karsten/index.html
+(defn karsten []
+  (html
+   [:head 
+    [:title "Karsten Lang"]
+    [:style 
+     {:type "text/css"}
+     "<!-- A:link {text-decoration: none}A:visited{text-decoration:none}A:active{text-decoration:none}-->" ]]
+   [:body {:bgcolor "#000000" :text "#eeeeee" :link "#ffffff" :vlink "#dddddd" :alink "#ffffff"}
+    [:p {:align "center"}
+     [:img {:src "/karsten/pic/klangsign.small.gif" :width "100" :height "147"}]
+     [:img {:src "/karsten/pic/klangintro.small.gif" :width "100" :height "147"}]
+     [:img {:src "/karsten/pic/logo.small.gif" :width "100" :height "147"}]
+     [:br]
+     [:a {:href "http://google.com"} "Karsten Lang"]
+     [:br]
+     [:br]
+     [:a {:href "/karsten/computer/" :onMouseOver "statusText('About my computer')"} "computer"] " - "
+     [:a {:href "/karsten/cv/" :onMouseOver "statusText('Curriculum Vitae')"} "curriculum vitae"]
+     [:br]
+     [:a {:href "/karsten/books/" :onMouseOver "statusText('the books I read')"} "books"] " - "
+     [:a {:href "/karsten/movies/" :onMouseOver "statusText('the movies I see')"} "movies"] " - "
+     [:a {:href "/karsten/music/" :onMouseOver "statusText('the music I hear')"} "music"] " - "
+     [:a {:href "/karsten/life/" :onMouseOver "statusText('life in a So-Called Space Age')"} "life"]
+     [:br]
+     (statusText)
+     [:em 
+      "The only true freedom is freedom from the hearts desires" [:br]
+      "&amp; the only true happiness....this way lies." [:br]]
+     [:a {:href "/karsten/music/dusk.html#quote"
+	  :onMouseOver "statusText('The lyrics from TheThe - DUSK')"}
+      "Th" [:u "e"] "th" [:u "e"]]
+     (last-changed)
+     ]]))
 
 (defn hello-world []   
   (html [:head 
@@ -11,16 +58,6 @@
   (html [:head 
 	 [:title "you were eaten by a grue"]]
 	[:body [:h1 "you were eaten by a grue"]]))
-
-(defn last-changed []
-  (html [:script {:language "JavaScript"} 
-	 "document.write(\"<hr><center><h6><i>Last changed on \"+ document.lastModified + \"</i></h6></center>\");"]))
-
-(defn statusText []
-    (html [:script {:language "JavaScript"} 
-	   "function statusText(text) { setTimeout('top.window.status=\"'+text+'\";',1); }"]
-	  )
-    )
 
 (defroutes lang
   (GET "/karsten" (karsten))
