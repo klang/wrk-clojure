2ddaeaf8ac54aad6a2ba6505898f2fc7f23d1174
Karsten Lang <karstenlang@gmail.com>
2009-10-17 19:05:45 +0200

    finished

--- examples/p448.clj
+++ examples/p448.clj
@@ -18,11 +18,38 @@
   (first (index-filter pred coll)))
 
 (test #'index-of-any)
-
+(comment
 (defn #^{:test (fn []
 		 (assert (nil? (busted))))}
   busted [] "busted")
+)
+
 (test #'busted)
 
 (use '[clojure.contrib.test-is])
 
+(deftest test-index-of-any-with-nil-args
+  (is (nil? (index-of-any #{\a} nil)))
+  (is (nil? (index-of-any nil "foo"))))
+
+(deftest test-index-of-any-with-empty-args
+  (is (nil? (index-of-any #{\a} "")))
+  (is (nil? (index-of-any #{} "foo"))))
+
+(deftest test-index-of-any-with-match
+  (is (zero? (index-of-any #{\z \a} "zzabyycdxx")))
+  (is (= 3 (index-of-any #{\b \y} "zzabyycdxx"))))
+
+(deftest test-index-of-any-with-without-match
+  (is (nil? (index-of-any #{\z} "aba"))))
+
+(run-tests)
+
+(deftest test-that-demonstrates-failure
+  (is (= 5 (+ 2 2))))
+(deftest test-that-demonstrates-error-message
+  (is (= 3 Math/PI) "PI is an integer?"))
+(deftest test-divide-by-zero
+  (is (thrown? ArithmeticException (/ 5 0))))
+
+(run-tests)
