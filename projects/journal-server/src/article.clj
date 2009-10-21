68229749ec74d61823e99e2a15b79680fa2da7f9
Karsten Lang <karstenlang@gmail.com>
2009-10-21 22:58:28 +0200

    not done

--- projects/journal-server/src/article.clj
+++ projects/journal-server/src/article.clj
@@ -1,9 +1,12 @@
-create table article (
-     id           serial primary key,
-     title        text not null unique check (trim(title)  ''),
-     description  text not null default '',
-     body         text not null default '',
-     created      timestamp not null default now(),
-     updated      timestamp not null default now(),
-     published    timestamp null default null
-);
+
+(defn create-article []
+  (create-table :article
+		[:id           :serial primary key]
+		[:title        :text "NOT NULL" unique check (trim(title)  '')]
+		[:description  :text "NOT NULL" default '']
+		[:body         :text "NOT NULL" default '']
+		[:created      :timestamp "NOT NULL" default now()]
+		[:updated      :timestamp "NOT NULL" default now()]
+		[:published    :timestamp "NULL" default null]))
+
+
