6d6d5e1fd326f1f5b7c6e3e0f2416df1de030cff
Karsten Lang <karstenlang@gmail.com>
2009-10-21 23:02:27 +0200

    not done

--- projects/journal-server/src/article.clj
+++ projects/journal-server/src/article.clj
@@ -1,12 +1,12 @@
 
 (defn create-article []
   (create-table :article
-		[:id           :serial primary key]
-		[:title        :text "NOT NULL" unique check (trim(title)  '')]
-		[:description  :text "NOT NULL" default '']
-		[:body         :text "NOT NULL" default '']
-		[:created      :timestamp "NOT NULL" default now()]
-		[:updated      :timestamp "NOT NULL" default now()]
-		[:published    :timestamp "NULL" default null]))
+		[:id           :serial "primary key"]
+		[:title        :text "NOT NULL" "unique check (trim(title)  '')"]
+		[:description  :text "NOT NULL" "default ''"]
+		[:body         :text "NOT NULL" "default ''"]
+		[:created      :timestamp "NOT NULL" "default now()"]
+		[:updated      :timestamp "NOT NULL" "default now()"]
+		[:published    :timestamp "NULL" "default null"]))
 
 
