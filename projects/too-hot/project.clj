8f57a2704d5d882c9488cde67d6c4c0620c333db
Karsten Lang Pedersen <klang@feersum.(none)>
2009-12-03 22:55:41 +0100

    testing out leiningen and clojars

---
+++ projects/too-hot/project.clj
@@ -0,0 +1,4 @@
+(defproject org.clojars.klang.too-hot "1.0.0"
+  :description "A very simple Fahrenheit to Celsius converter."
+  :dependencies [[org.clojure/clojure "1.1.0-alpha-SNAPSHOT"]]
+  :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]])
---
+++ projects/too-hot/src/too_hot.clj
@@ -0,0 +1,12 @@
+(ns too-hot)
+ 
+(defn celsius
+  "Converts a temperature in Fahrenheit to Celsius."
+  [f]
+  (* (- f 32) 5/9))
+ 
+(defn fahrenheit
+  "Converts a temperature in Celsius to Fahrenheit."
+  [c]
+  (+ (* c 9/5) 32))
+
