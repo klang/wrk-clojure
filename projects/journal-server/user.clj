4aa59f5fc95a8f9c391ef70197979633d58977ec
Karsten Lang Pedersen <klang@feersum.(none)>
2009-12-04 20:49:03 +0100

    this file is run by leiningen, so we move stuff we want to do to a comment

--- projects/journal-server/user.clj
+++ projects/journal-server/user.clj
@@ -1,5 +1,10 @@
-(use 'clojure.contrib.repl-utils)
-(load-file "./src/article.clj")
-(load-file "./src/site.clj")
-(load-file "./src/start-server.clj")
-;(load-file "./src/eriklavigne.clj")
+;; this file is run by leiningen
+(println "use comment in user.clj")
+(comment
+  (use 'clojure.contrib.repl-utils)
+  (load-file "./src/article.clj")
+  (load-file "./src/site.clj")
+  (load-file "./src/start-server.clj")
+  (load-file "./src/eriklavigne.clj")
+)
+
